6c43d37f5d4f15632026a38415cba7aa8d85c3de
Paulo Alexandre <paulofore@gmail.com>
2018-08-21 20:11:57 -0300

    Gerando codigo cookie

--- common/models/visitante.js
+++ common/models/visitante.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const crypto = require('crypto');
+
 module.exports = function (Visitante) {
     /**
      * retorna o proximo cookie
@@ -7,8 +9,10 @@ module.exports = function (Visitante) {
      */
 
     Visitante.proximoCookie = function (callback) {
-        var codigoCookie =  '123456';
-        // TODO
+        var current_date = (new Date()).valueOf().toString();
+        var random = Math.random().toString();
+        var codigoCookie = crypto.createHash('sha1').update(current_date + random).digest('hex');
         callback(null, codigoCookie);
     };
 };
+    
