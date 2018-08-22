e565d31a44c6783a191c596c962741f0101e0ab5
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-08-22 10:10:34 -0300

    tratando data e explorer

--- common/models/registro-interesse.js
+++ common/models/registro-interesse.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Registrointeresse) {
 
+
+    Registrointeresse.observe('before save', function updateTimestamp(ctx, next) {
+        if (ctx.instance) {
+            ctx.instance.dataHora = new Date();
+        } else {
+            ctx.data.dataHora = new Date();
+        }
+        next();
+    });
 };
--- common/models/visitante.js
+++ common/models/visitante.js
@@ -14,5 +14,14 @@ module.exports = function (Visitante) {
         var codigoCookie = crypto.createHash('sha1').update(current_date + random).digest('hex');
         callback(null, codigoCookie);
     };
+
+
+    Visitante.observe('before save', function updateTimestamp(ctx, next) {
+        if (ctx.instance) {
+            ctx.instance.dataHora = new Date();
+        } else {
+            ctx.data.dataHora = new Date();
+        }
+        next();
+    });
 };
-    
