a701da169b78cac4a373f34317d180168b4884a8
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-08-23 10:56:28 -0300

    com hash na paginavalidacao

--- common/models/pagina-validacao-web.js
+++ common/models/pagina-validacao-web.js
@@ -1,5 +1,20 @@
 'use strict';
 
-module.exports = function(Paginavalidacaoweb) {
+const crypto = require('crypto');
 
+module.exports = function (Paginavalidacaoweb) {
+
+    Paginavalidacaoweb.observe('before save', function updateHash(ctx, next) {
+        var current_date = (new Date()).valueOf().toString();
+        var random = Math.random().toString();
+        var codigoCookie = crypto.createHash('sha1').update(current_date + random).digest('hex');
+        if (ctx.isNewInstance) {
+            if (ctx.instance) {
+                ctx.instance.codigoHash = codigoCookie;
+            } else {
+                ctx.data.codigoHash = codigoCookie;
+            }
+        }
+        next();
+    });
 };
