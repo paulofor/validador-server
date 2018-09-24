5f409ca0d1e6a643d95b08f02ce92a16a0b5ca0c
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-09-24 10:25:49 -0300

    Metodo Remoto Flexivel

--- common/models/projeto-canvas-my-sql.js
+++ common/models/projeto-canvas-my-sql.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Projetocanvasmysql) {
+module.exports = function (Projetocanvasmysql) {
+    /**
+     * Itens de modelos canvas que vao ajudar a construir as entidades do sistema gerador
+     * @param {number} idProjeto
+     * @param {Function(Error, array, array, array)} callback
+     */
 
+    Projetocanvasmysql.ItensAjudaEntidade = function (idProjeto, callback) {
+        var listaMvpCanvas, listaGanhoDorCanvas, listaProjetoCanvas;
+        // TODO
+        Projetocanvasmysql.findById(idProjeto, { "include": "mvpCanvasMySqls" }, function (err, modelInstance) {
+            //modelInstance has properties here and can be returned to
+            //the API call using the callback, for example:
+            callback(null, modelInstance.mvpCanvasMySqls, modelInstance.mvpCanvasMySqls, modelInstance.mvpCanvasMySqls);
+        });
+    }
 };
