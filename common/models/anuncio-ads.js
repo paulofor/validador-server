d91cbc9659da2d1bbd31b823ea19e135e8ea1195
Paulo Alexandre <paulofore@gmail.com>
2018-09-27 20:40:50 -0300

    metodo remoto para buscar anuncios de campanha

--- common/models/anuncio-ads.js
+++ common/models/anuncio-ads.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Anuncioads) {
 
+
+ /**
+ * 
+ * @param {number} idPagina 
+ * @param {Function(Error, array)} callback
+ */
+
+Anuncioads.paraCampanhaPorIdPagina = function(idPagina, callback) {
+    var listaAnuncioAds;
+    // TODO
+    callback(null, listaAnuncioAds);
+  };
 };
