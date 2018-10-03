df2acb5d25b7ee9dd20231b362a86a5fc1948c7f
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-10-03 13:07:50 -0300

    relacionamento_entidade

--- common/models/entidade.js
+++ common/models/entidade.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Entidade) {
+module.exports = function (Entidade) {
 
+
+    /**
+ *
+ * @param {number} idEntidade
+ * @param {Function(Error, array)} callback
+ */
+
+    Entidade.listaCompletaRelacionemnto = function (idEntidade, callback) {
+        var listaRelacionamento;
+        // TODO
+        callback(null, listaRelacionamento);
+    };
 };
