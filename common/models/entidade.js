2269ea4f4f5baab1620f57366ca99ccae836ee79
Paulo Alexandre <paulofore@gmail.com>
2018-10-06 09:56:24 -0300

    relacionamentos por entidade

--- common/models/entidade.js
+++ common/models/entidade.js
@@ -3,17 +3,4 @@
 module.exports = function (Entidade) {
 
 
-    /**
- *
- * @param {number} idEntidade
- * @param {Function(Error, array)} callback
- */
-
-    Entidade.listaCompletaRelacionemnto = function (idEntidade, callback) {
-        var listaRelacionamento;
-        Entidade.findById(idEntidade,{"include": [{ "relation": "rel1" , "scope" : {"include" : "entidade2"}}, { "relation": "rel2" }]}, (err, result) => {
-            listaRelacionamento = result;
-            callback(null, listaRelacionamento);
-        })
-    };
 };
--- common/models/relacionamento-entidade.js
+++ common/models/relacionamento-entidade.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function(Relacionamentoentidade) {
+module.exports = function (Relacionamentoentidade) {
+
+    /**
+     * 
+     * @param {number} idEntidade 
+     * @param {Function(Error, array)} callback
+     */
+
+    Relacionamentoentidade.listaCompletaPorEntidade = function (idEntidade, callback) {
+        var listaRelacionamento;
+        Relacionamentoentidade.find({"where" : { "or" : [{"id_entidade1" : idEntidade},{"id_entidade2":idEntidade}]} , "include" : ["entidade2" , "entidade1"] }, (err, result) => {
+            listaRelacionamento = result;
+            callback(null, listaRelacionamento);
+        })
+    };
+
+
 
 };
