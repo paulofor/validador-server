58d122a862872f11cb46136346dddb5771af48a7
Paulo Alexandre <paulofore@gmail.com>
2018-10-08 21:43:37 -0300

    chamadas facade

--- common/models/anuncio-ads.js
+++ common/models/anuncio-ads.js
@@ -1,17 +1,26 @@
 'use strict';
 
-module.exports = function(Anuncioads) {
+module.exports = function (Anuncioads) {
 
 
- /**
- * 
- * @param {number} idPagina 
- * @param {Function(Error, array)} callback
- */
+  /**
+  * 
+  * @param {number} idPagina 
+  * @param {Function(Error, array)} callback
+  */
 
-Anuncioads.paraCampanhaPorIdPagina = function(idPagina, callback) {
-    var listaAnuncioAds;
-    // TODO
-    callback(null, listaAnuncioAds);
+  Anuncioads.paraCampanhaPorIdPagina = function (idPagina, callback) {
+    var listaAnuncioAds = [];
+    var ds = Anuncioads.dataSource;
+    var sql = "select AnuncioAds.* " +
+      " from AnuncioAds " +
+      " inner join ItemValidacaoPagina on ItemValidacaoPagina.projetoCanvasMySqlId = AnuncioAds.projetoCanvasMySqlId " +
+      " where paginaValidacaoWebId = " + idPagina;
+    ds.connector.query(sql, function (err, result) {
+      if (err) console.error(err);
+      else listaAnuncioAds = result;
+      callback(err, listaAnuncioAds);
+    });
+    
   };
 };
--- common/models/palavra-chave-ads.js
+++ common/models/palavra-chave-ads.js
@@ -1,5 +1,26 @@
 'use strict';
 
-module.exports = function(Palavrachaveads) {
+var app = require('../../server/server');
+
+module.exports = function (Palavrachaveads) {
+
+
+
+    /**
+ * 
+ * @param {number} idPagina 
+ * @param {Function(Error, array)} callback
+ */
+
+    Palavrachaveads.ParaCampanhaPorIdPagina = function (idPagina, callback) {
+        var Anuncioads = app.models.AnuncioAds;
+        Anuncioads.paraCampanhaPorIdPagina(idPagina), ((err, result) => {
+            console.log('Result:', JSON.stringify(result));
+            var listaPalavra;
+            // TODO
+            callback(null, listaPalavra);
+        })
+
+    };
 
 };
