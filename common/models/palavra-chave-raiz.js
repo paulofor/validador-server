b9d0e9b183cc6fe77f47a9c73393a332974f02d0
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-11-19 10:46:40 -0200

    palavras chaves

--- common/models/palavra-chave-raiz.js
+++ common/models/palavra-chave-raiz.js
@@ -3,6 +3,21 @@
 module.exports = function (Palavrachaveraiz) {
 
     /**
+     * Obtem o objeto com a lista de melhores baseada nos indices da funcao ListaMelhoresPadrao no objeto PalavraChaveEstatistica
+     * @param {number} idPalavraChaveRaiz
+     * @param {Function(Error, object)} callback
+     */
+
+    Palavrachaveraiz.ObtemComListaMelhoresPadrao = function (idPalavraChaveRaiz, callback) {
+        var raizComMelhores;
+        // TODO
+        callback(null, raizComMelhores);
+    };
+
+
+
+
+    /**
      * Lista para ir no Ads Google pesquisar
      * @param {Function(Error, array)} callback
      */
@@ -12,15 +27,17 @@ module.exports = function (Palavrachaveraiz) {
         var d = new Date();
         d.setDate(d.getDate() - 20); // 20 dias 
         Palavrachaveraiz.find({
-            "where": { "and" : 
-                [ 
-                    { "ativo": 1 }, 
-                    { "or" : [
-                        {"dataUltimaAtualizacao" : { "lt": d } },
-                        {"dataUltimaAtualizacao" : null} 
+            "where": {
+                "and":
+                    [
+                        { "ativo": 1 },
+                        {
+                            "or": [
+                                { "dataUltimaAtualizacao": { "lt": d } },
+                                { "dataUltimaAtualizacao": null }
+                            ]
+                        }
                     ]
-                    }
-                ] 
             }
         },
             callback);
@@ -28,7 +45,7 @@ module.exports = function (Palavrachaveraiz) {
 
 
     Palavrachaveraiz.ListaParaConsultaTeste = function (callback) {
-        Palavrachaveraiz.find({"where" : {"id" : 1}} , callback);
+        Palavrachaveraiz.find({ "where": { "id": 1 } }, callback);
     };
 
 };
