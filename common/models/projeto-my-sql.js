0ee79a1f5b25fccd558c221dea4b57ac31757509
Paulo Alexandre <paulofore@gmail.com>
2018-11-26 20:54:35 -0200

    trecho de projeto retornando somente string

--- common/models/projeto-my-sql.js
+++ common/models/projeto-my-sql.js
@@ -1,65 +1,87 @@
 'use strict';
 
-module.exports = function(Projetomysql) {
+module.exports = function (Projetomysql) {
+
+    /**
+     * Pesquisa retornando apenas strings
+     * @param {string} trecho 
+     * @param {Function(Error, array)} callback
+     */
+
+    Projetomysql.PesquisaPorTrechoStr = function (trecho, callback) {
+        if (trecho.length <= 3) {
+            callback(null, [])
+        } else {
+            var lista = [];
+            var ds = Projetomysql.dataSource;
+            var sql = "select ProjetoMySql.nome " +
+                " from ProjetoMySql " +
+                " where UPPER(nome) like '%" + trecho.toUpperCase() + "%' ";
+            ds.connector.query(sql, (err, result) => {
+                result.map((item) => {
+                    lista.push(item.nome)
+                })
+                callback(err, lista);
+            });
+        }
+    };
+
+    /**
+     * Pesquisa por trecho no nome do projeto
+     * @param {string} trecho 
+     * @param {Function(Error, array)} callback
+     */
+
+    Projetomysql.PesquisaPorTrecho = function (trecho, callback) {
+        var ds = Projetomysql.dataSource;
+        var sql = "select ProjetoMySql.* " +
+            " from ProjetoMySql " +
+            " where UPPER(nome) like '%" + trecho.toUpperCase() + "%' ";
+        ds.connector.query(sql, callback);
+    };
 
 
 
-/**
- * Pesquisa por trecho no nome do projeto
- * @param {string} trecho 
- * @param {Function(Error, array)} callback
- */
+    /**
+     * Retorna todos os projetos que ainda estão na fase de serem associados com palavras chaves ainda em momento inicial usando codigo de etapa = IDEA
+     * @param {Function(Error, array)} callback
+     */
 
-Projetomysql.PesquisaPorTrecho = function(trecho, callback) {
-    var ds = Projetomysql.dataSource;
-    var sql = "select ProjetoMySql.* " +
-      " from ProjetoMySql " +
-      " where UPPER(nome) like '%" + trecho.toUpperCase() + "%' ";
-    ds.connector.query(sql, callback);
-  };
+    Projetomysql.ListaIdeiaBase = function (callback) {
+        //let filtro = { "include" : {"relation" : "etapaProjeto" , 
+        //                        "scope" : { "where" : { "codigo" : "IDEA" }}}};
+        //Projetomysql.find(this.filtro, callback);
 
-
-
-/**
- * Retorna todos os projetos que ainda estão na fase de serem associados com palavras chaves ainda em momento inicial usando codigo de etapa = IDEA
- * @param {Function(Error, array)} callback
- */
-
-Projetomysql.ListaIdeiaBase = function(callback) {
-    //let filtro = { "include" : {"relation" : "etapaProjeto" , 
-    //                        "scope" : { "where" : { "codigo" : "IDEA" }}}};
-    //Projetomysql.find(this.filtro, callback);
-
-    // Usar o Json é perigoso pq o raciocionio é diferente e arriscado.
-    var ds = Projetomysql.dataSource;
-    var sql = "select ProjetoMySql.* " +
-      " from ProjetoMySql " +
-      " inner join EtapaProjeto on EtapaProjeto.id = ProjetoMySql.etapaProjetoId " +
-      " where EtapaProjeto.codigo = 'IDEA' ";
-    ds.connector.query(sql, callback);
-  };
-  
-  
+        // Usar o Json é perigoso pq o raciocionio é diferente e arriscado.
+        var ds = Projetomysql.dataSource;
+        var sql = "select ProjetoMySql.* " +
+            " from ProjetoMySql " +
+            " inner join EtapaProjeto on EtapaProjeto.id = ProjetoMySql.etapaProjetoId " +
+            " where EtapaProjeto.codigo = 'IDEA' ";
+        ds.connector.query(sql, callback);
+    };
 
 
- /**
-     * Itens de modelos canvas que vao ajudar a construir as entidades do sistema gerador
-     * @param {number} idProjeto
-     * @param {Function(Error, array, array, array)} callback
-      */
 
-     Projetomysql.ItensAjudaEntidade = function (idProjeto, callback) {
-        var listaMvpCanvas = [], listaGanhoDorCanvas  = [], listaProjetoCanvas = [];
 
-        Projetomysql.findById(idProjeto, {"include" : {"relation": "mvpCanvasMySqls", "scope" : {"where" : { "or" : [{"tipo":"JORNADA"}] }}}}, function (err, modelInstance) {
+    /**
+        * Itens de modelos canvas que vao ajudar a construir as entidades do sistema gerador
+        * @param {number} idProjeto
+        * @param {Function(Error, array, array, array)} callback
+         */
+
+    Projetomysql.ItensAjudaEntidade = function (idProjeto, callback) {
+        var listaMvpCanvas = [], listaGanhoDorCanvas = [], listaProjetoCanvas = [];
+
+        Projetomysql.findById(idProjeto, { "include": { "relation": "mvpCanvasMySqls", "scope": { "where": { "or": [{ "tipo": "JORNADA" }] } } } }, function (err, modelInstance) {
             console.log(modelInstance);
             console.log(err);
             if (!err) {
                 listaMvpCanvas = modelInstance.toJSON().mvpCanvasMySqls;
-                Projetomysql.findById(idProjeto, {"include" : {"relation": "ganhoDorCanvasMySqls", "scope" : {"where" : {"tipo":"PRODUTO_SERVICO"} }}}, (err, result) => {
+                Projetomysql.findById(idProjeto, { "include": { "relation": "ganhoDorCanvasMySqls", "scope": { "where": { "tipo": "PRODUTO_SERVICO" } } } }, (err, result) => {
                     if (!err) {
                         listaGanhoDorCanvas = result.toJSON().ganhoDorCanvasMySqls;
-                        Projetomysql.findById(idProjeto, {"include" : {"relation": "projetoCanvasMySqls", "scope" : {"where" : {"tipo":"VALOR"} }}} , (err,result) => {
+                        Projetomysql.findById(idProjeto, { "include": { "relation": "projetoCanvasMySqls", "scope": { "where": { "tipo": "VALOR" } } } }, (err, result) => {
                             if (!err) {
                                 listaProjetoCanvas = result.toJSON().projetoCanvasMySqls;
                                 callback(null, listaMvpCanvas, listaGanhoDorCanvas, listaProjetoCanvas);
@@ -74,7 +96,7 @@ Projetomysql.ListaIdeiaBase = function(callback) {
             } else {
                 callback(null, listaMvpCanvas, listaGanhoDorCanvas, listaProjetoCanvas);
             }
-            
+
         });
     }
 };
