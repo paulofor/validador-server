a0dc3c06021706ac1ddbaf278ff42d2a54c9d69d
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2018-11-30 10:11:54 -0200

    evolucao

--- common/models/palavra-chave-google.js
+++ common/models/palavra-chave-google.js
@@ -6,22 +6,7 @@ var app = require('../../server/server');
 module.exports = function(Palavrachavegoogle) {
 
 
-/**
- * se receber um projeto com id 0 cria o projeto e o relacionamento senão cria somente o relacionamento
- * @param {object} relacionamentoComProjeto 
- * @param {Function(Error)} callback
- */
 
-Palavrachavegoogle.InsereProjetoRelacionamento = function(relacionamentoComProjeto, callback) {
-    if (relacionamentoComProjeto.projetoMySql.id==0) {
-        app.models.ProjetoMySql.create(relacionamentoComProjeto.projetoMySql, (err, result) => {
-            relacionamentoComProjeto.projetoMySqlId = result.id;
-            Palavrachavegoogle.create(relacionamentoComProjeto, callback); 
-        })
-    } else {
-        Palavrachavegoogle.create(relacionamentoComProjeto, callback); 
-    }
-  };
   
 
 
--- common/models/palavra-google-projeto.js
+++ common/models/palavra-google-projeto.js
@@ -2,4 +2,25 @@
 
 module.exports = function(Palavragoogleprojeto) {
 
+
+/**
+ * se receber um projeto com id 0 cria o projeto e o relacionamento senão cria somente o relacionamento
+ * @param {object} relacionamentoComProjeto 
+ * @param {Function(Error)} callback
+ */
+
+Palavragoogleprojeto.InsereProjetoRelacionamento = function(relacionamentoComProjeto, projeto, callback) {
+    console.log('Relacionamento:' , relacionamentoComProjeto);
+    console.log('Projeto:' , projeto);
+    if (projeto.id==0) {
+        app.models.ProjetoMySql.create(projeto, (err, result) => {
+            relacionamentoComProjeto.projetoMySqlId = result.id;
+            Palavragoogleprojeto.create(relacionamentoComProjeto, callback); 
+        })
+    } else {
+        relacionamentoComProjeto.projetoMySqlId = projeto.id;
+        Palavragoogleprojeto.create(relacionamentoComProjeto, callback); 
+    }
+  };
+
 };
