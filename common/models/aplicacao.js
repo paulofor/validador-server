2f14ede47efac84b2f55fd86901b34d72dea6285
Paulo Alexandre <paulofore@gmail.com>
2018-12-03 22:11:20 -0200

    tratamento da lista de componentes de uma aplicacao

--- common/models/aplicacao.js
+++ common/models/aplicacao.js
@@ -1,5 +1,33 @@
 'use strict';
 
-module.exports = function(Aplicacao) {
+var app = require('../../server/server');
+
+module.exports = function (Aplicacao) {
+
+
+    /**
+     * Combina tipo componente com entidades para gerar os componentes web
+     * @param {number} idAplicacao 
+     * @param {Function(Error)} callback
+     */
+
+    Aplicacao.AtualizaComponenteWeb = function (idAplicacao, callback) {
+        console.log('idAplicacao: ' , idAplicacao);
+        app.models.entidade.find({"where" : {"id_aplicacao" : idAplicacao } }, (err, result) => {
+            console.log('Erro(1): ' , err);
+            console.log('Lista Entidade: ' , result);
+            result.map((item) => {
+                trataItem(ds, item);
+            })
+        })
+        
+        callback(null);
+    }
+
+    function trataItem(entidade) {
+        app.models.TipoComponenteWeb.find((err, result) => {
+            console.log('Lista Tipo Componente: ' , result);
+        })
+    }
 
 };
