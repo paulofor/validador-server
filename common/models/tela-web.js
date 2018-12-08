5f68d4d6541dd70374844c1985a7ca89513be1d6
Paulo Alexandre <paulofore@gmail.com>
2018-12-08 20:41:00 -0200

    telas

--- common/models/tela-web.js
+++ common/models/tela-web.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Telaweb) {
+module.exports = function (Telaweb) {
+
+    /**
+     * Obtem todas as telas de uma aplicacao com os objetos internos necessarios no gerador de codigo
+     * @param {number} idAplicacao 
+     * @param {Function(Error, array)} callback
+     */
+
+    Telaweb.ObtemPorIdAplicacaoParaGerador = function (idAplicacao, callback) {
+        var filtro = { 
+            "where" : {"aplicacaoId" : idAplicacao} ,
+            "include" : {"relation" : "telaComponenteWebs" , "scope" : { "include" : "componenteWeb" } }
+        };
+        Telaweb.find(filtro, callback);
+    };
 
 };
