847644cc42464b909927cc5990077221a4e02bf4
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-01-28 13:25:38 -0200

    planejamento

--- common/models/processo-negocio.js
+++ common/models/processo-negocio.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Processonegocio) {
+module.exports = function (Processonegocio) {
 
+
+
+    /**
+ * Obtem lista de processo com filtro de contexto e semana
+ * @param {number} idSemana
+ * @param {string} contexto
+ * @param {Function(Error, array)} callback
+ */
+
+    Processonegocio.ObtemComPlanoPorSemana = function (idSemana, contexto, callback) {
+        var listaProcesso;
+        // TODO
+        callback(null, listaProcesso);
+    };
 };
--- common/models/semana.js
+++ common/models/semana.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Semana) {
+module.exports = function (Semana) {
 
+
+ /**
+ *
+ * @param {number} semanas Quantidade de semanas pode ser positiva ou negativa
+ * @param {Function(Error, object)} callback
+ */
+
+    Semana.ObtemDeslocada = function (semanas, callback) {
+        var semana;
+        // TODO
+        callback(null, semana);
+    };
 };
