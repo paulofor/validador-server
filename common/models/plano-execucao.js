fff15e22dcde8c9f476256a004a97a479437ce64
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-01-31 11:41:43 -0200

    suporte de dados

--- common/models/plano-execucao.js
+++ common/models/plano-execucao.js
@@ -1,5 +1,16 @@
 'use strict';
 
-module.exports = function(Planoexecucao) {
+module.exports = function (Planoexecucao) {
 
+
+    /**
+    * Cria por um processo todas as semanas existentes
+    * @param {number} idProcessoNegocio
+    * @param {Function(Error)} callback
+    */
+
+    Planoexecucao.CriaPorProcesso = function (idProcessoNegocio, callback) {
+        // TODO
+        callback(null);
+    };
 };
--- common/models/semana.js
+++ common/models/semana.js
@@ -3,19 +3,34 @@
 module.exports = function (Semana) {
 
 
- /**
- *
- * @param {number} semanas Quantidade de semanas pode ser positiva ou negativa
- * @param {Function(Error, object)} callback
- */
+    /**
+    *
+    * @param {number} semanas Quantidade de semanas pode ser positiva ou negativa
+    * @param {Function(Error, object)} callback
+    */
 
     Semana.ObtemDeslocada = function (semanas, callback) {
         var dataRef = new Date();
-        dataRef = new Date(dataRef.setTime( dataRef.getTime() + (semanas * 7) * 86400000 ));
-        var filtro = { "where" : {"and" : [
-            { "PrimeiroDia" : {"lte" : dataRef } },
-            { "UltimoDia" : {"gte" : dataRef }}
-        ]} };
+        dataRef = new Date(dataRef.setTime(dataRef.getTime() + (semanas * 7) * 86400000));
+        var filtro = {
+            "where": {
+                "and": [
+                    { "PrimeiroDia": { "lte": dataRef } },
+                    { "UltimoDia": { "gte": dataRef } }
+                ]
+            }
+        };
         Semana.find(filtro, callback);
+    };
+
+
+    /**
+    * Cria todas as semanas de um ano
+    * @param {number} ano
+    * @param {Function(Error)} callback
+    */
+    Semana.CriaParaAno = function (ano, callback) {
+        // TODO
+        callback(null);
     };
 };
