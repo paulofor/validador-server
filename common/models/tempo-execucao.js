4350a17eb4cb4cd61baf4b915ce0c0e56070c631
Paulo Alexandre <paulofore@gmail.com>
2019-02-08 22:27:08 -0200

    ajuste tempo

--- common/models/tempo-execucao.js
+++ common/models/tempo-execucao.js
@@ -8,17 +8,27 @@ module.exports = function (Tempoexecucao) {
     * @param {object} execucao
     * @param {Function(Error)} callback
     */
-    Tempoexecucao.insere = function (execucao, callback) {
-        Tempoexecucao.create(execucao,callback);
+    Tempoexecucao.Insere = function (execucao, callback) {
+        //console.log('Execucao:' , JSON.stringify(execucao));
+        Tempoexecucao.create(execucao, callback);
+
     };
 
-    
+
     /**
     * Alteracao com calculos
     * @param {object} execucao
     * @param {Function(Error)} callback
     */
-    Tempoexecucao.Altera = function(execucao, callback) {
-        Tempoexecucao.update(execucao,callback);
+    Tempoexecucao.Altera = function (execucao, callback) {
+        //console.log('Execucao:' , JSON.stringify(execucao));
+        var ds = Tempoexecucao.dataSource;
+        Tempoexecucao.upsert(execucao, (result, err) => {
+            var sql = "update PlanoExecucao set TempoExecucao = " +
+                " (select  SEC_TO_TIME( SUM( TIME_TO_SEC( tempo ) ) ) from TempoExecucao " +
+                " where planoExecucaoId = " + execucao.planoExecucaoId + " ) " +
+                " where id = " + execucao.planoExecucaoId;
+            ds.connector.query(sql, callback)
+        });
     };
 };
