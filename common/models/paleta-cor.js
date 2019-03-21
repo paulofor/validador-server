af6c1c6c1cca3553d6f421508fc4f7729a0fd9c4
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-03-21 13:03:41 -0300

    paleta-cor

--- common/models/paleta-cor.js
+++ common/models/paleta-cor.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Paletacor) {
+
+
+module.exports = function (Paletacor) {
+
+
+    /**
+    *
+    * @param {number} idAplicacao
+    * @param {Function(Error, object)} callback
+    */
+    Paletacor.ObtemPorIdAplicacao = function (idAplicacao, callback) {
+        Paletacor.findById(1,callback);
+    };
+
+
 
 };
