be2b807e0ef4d20007bb0e2a09f384ffb870d2d9
Paulo Alexandre <paulofore@gmail.com>
2019-03-22 21:22:16 -0300

    cores

--- common/models/paleta-cor.js
+++ common/models/paleta-cor.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+var app = require('../../server/server');
 
 module.exports = function (Paletacor) {
 
@@ -11,7 +11,13 @@ module.exports = function (Paletacor) {
     * @param {Function(Error, object)} callback
     */
     Paletacor.ObtemPorIdAplicacao = function (idAplicacao, callback) {
-        Paletacor.findById(1,callback);
+        app.models.Aplicacao.findById(idAplicacao, (err, result) => {
+            if (result.paletaCorsId) {
+                Paletacor.findById(result.paletaCorsId, callback);
+            } else {
+                callback(null, null);
+            }
+        })
     };
 
 
