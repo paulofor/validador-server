109ec1dbef9e6d3dec487b00293e626217b07e09
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-04-22 17:31:09 -0300

    container

--- common/models/container.js
+++ common/models/container.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = function(Container) {
+module.exports = function (Container) {
+
+    app.dataSources.storage.connector.getFilename = function (file, req, res) {
+        //file.name is original filename uploaded
+        var filename = req.query.filename || 'general.ext';
+        return filename;
+    };
 
 };
