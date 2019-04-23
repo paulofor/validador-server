caa79ba04047bfa527df0fb65ca1747c4a18f792
Paulo Alexandre <paulofore@gmail.com>
2019-04-23 19:59:44 -0300

    imagens

--- common/models/container.js
+++ common/models/container.js
@@ -1,10 +1,11 @@
 'use strict';
 
+var app = require('../../server/server');
+
 module.exports = function (Container) {
 
     app.dataSources.storage.connector.getFilename = function (file, req, res) {
-        //file.name is original filename uploaded
-        var filename = req.query.filename || 'general.ext';
+        var filename =  req.query.nome + '_' + (new Date()).valueOf().toString() + '.jpg';
         return filename;
     };
 
