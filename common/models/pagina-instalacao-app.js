acf7018aea1ab884f6fed3110829fc4ec8141986
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-05-30 13:20:08 -0300

    pagina instalacao

--- common/models/pagina-instalacao-app.js
+++ common/models/pagina-instalacao-app.js
@@ -2,4 +2,18 @@
 
 module.exports = function(Paginainstalacaoapp) {
 
+
+    Paginainstalacaoapp.observe('before save', function updateHash(ctx, next) {
+        var current_date = (new Date()).valueOf().toString();
+        var random = Math.random().toString();
+        var codigoCookie = crypto.createHash('sha1').update(current_date + random).digest('hex');
+        if (ctx.isNewInstance) {
+            if (ctx.instance) {
+                ctx.instance.codigoHash = codigoCookie;
+            } else {
+                ctx.data.codigoHash = codigoCookie;
+            }
+        }
+        next();
+    });
 };
