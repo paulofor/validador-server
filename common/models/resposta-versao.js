9de7ca739b273c9135f674823829d8dda130281d
Paulo Alexandre <paulofore@gmail.com>
2019-07-04 20:46:07 -0300

    data na resposta versao

--- common/models/resposta-versao.js
+++ common/models/resposta-versao.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Respostaversao) {
 
+
+    Respostaversao.observe('before save', function updateTimestamp(ctx, next) {
+        if (ctx.instance) {
+            ctx.instance.dataHora = new Date();
+        } else {
+            ctx.data.dataHora = new Date();
+        }
+        next();
+    });
 };
