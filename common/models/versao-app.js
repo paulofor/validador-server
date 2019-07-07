44592420a97fb612e3170359320b2e9797b361e2
Paulo Alexandre <paulofore@gmail.com>
2019-07-06 21:45:11 -0300

    visitantes e respostas

--- common/models/versao-app.js
+++ common/models/versao-app.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Versaoapp) {
 
+
+
+    Versaoapp.observe('before save', function updateTimestamp(ctx, next) {
+        if (ctx.instance) {
+            ctx.instance.dataCriacao = new Date();
+        } else {
+            ctx.data.dataCriacao = new Date();
+        }
+        next();
+    });
+    
+
 };
