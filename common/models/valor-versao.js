034a2b732c9a7357d13bf02ffb0672db0aa05df6
Paulo Alexandre <paulofore@gmail.com>
2019-09-26 20:43:15 -0300

    versao app

--- common/models/valor-versao.js
+++ common/models/valor-versao.js
@@ -2,4 +2,31 @@
 
 module.exports = function(Valorversao) {
 
+ /**
+ * Atualizacao do relacionamento de um versao app
+ * @param {number} idVersaoApp 
+ * @param {array} listaRelacionamento 
+ * @param {Function(Error, object)} callback
+ */
+
+Valorversao.AtualizaPorVersaoApp = function(idVersaoApp, listaRelacionamento, callback) {
+
+    var sqlDelete = "delete from ValorVersao where versaoAppId = " + idVersaoApp;
+    var ds = Valorversao.dataSource;
+    ds.connector.query(sqlDelete, (err1, result1) => {
+        //console.log('Retorno 1: ' , result , " - Erro: " , )
+        if (err1) {
+            callback(err1, null);
+            return;
+        }
+        listaRelacionamento.forEach((etapa) => {
+            if (etapa.processoNegocioEtapaProjetos != null) {
+                delete etapa.processoNegocioEtapaProjetos.id;
+                Processonegocioetapaprojeto.create(etapa.processoNegocioEtapaProjetos);
+            }
+        });
+        callback(null, { 'result': 'ok' });
+    });
+  };
+  
 };
