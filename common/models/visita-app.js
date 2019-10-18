c25f7e2d2e74c30417c539e387b258481a68f7a1
tr626987 <tr626987@oi.corp.net>
2019-10-18 15:11:01 -0300

    geral

--- common/models/visita-app.js
+++ common/models/visita-app.js
@@ -1,5 +1,55 @@
 'use strict';
 
-module.exports = function(Visitaapp) {
+var app = require('../../server/server');
+
+module.exports = function (Visitaapp) {
+
+    /**
+     *
+     * @param {string} chaveUsuario
+     * @param {string} chavePagina
+     * @param {Function(Error, object)} callback
+     */
+    Visitaapp.RegistraVisitaTelaApp = function (chaveUsuario, chavePagina, callback) {
+        var resultado = new Object();
+        app.models.UsuarioProduto.find({'chave' : chaveUsuario} , (err1,result1) => {
+            if (err1) {
+                callback(err1,null);
+                return;
+            }
+            resultado.usuarioProdutoId = result1.id;
+            app.models.TelaApp.findOne({'chave' : chavePagina }, (err2,result2) => {
+                if (err2) {
+                    callback(err2,null);
+                    return;
+                }
+                resultado.telaAppId = result2.id;
+                resultado.dataHora = new Date();
+                Visitaapp.create(resultado,callback);
+            })
+           
+        })
+    };
+
+
+  /**
+   *
+   * @param {string} chaveUsuario
+   * @param {string} chaveVersaoApp
+   * @param {Function(Error, object)} callback
+   */
+    Visitaapp.RegistraVisitaVersaoApp = function (chaveUsuario, chaveVersaoApp, callback) {
+        var resultado = new Object();
+        app.models.UsuarioProduto.find({'chave' : chaveUsuario} , (err1,result1) => {
+            if (err1) {
+                callback(err1,null);
+                return;
+            }
+            resultado.usuarioProdutoId = result1.id;
+            resultado.versaoAppId = chaveVersaoApp;
+            resultado.dataHora = new Date();
+            Visitaapp.create(resultado,callback);
+        })
+    };
 
 };
