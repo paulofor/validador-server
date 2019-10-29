477152fccd93edcd6f6286f4d781a2a5da786f0c
tr626987 <tr626987@oi.corp.net>
2019-10-29 15:32:58 -0300

    calculando medias

--- common/models/anuncio-aplicativo.js
+++ common/models/anuncio-aplicativo.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Anuncioaplicativo) {
+module.exports = function (Anuncioaplicativo) {
+
+    /**
+     *
+     * @param {Function(Error, string)} callback
+     */
+
+    Anuncioaplicativo.CalculaMedias = function (callback) {
+        var sql2 = " update AnuncioAplicativo " +
+            " set mediaImpressao = (select avg(quantidadeImpressao) from CampanhaAds where CampanhaAds.anuncioAplicativoId = AnuncioAplicativo.id) ";
+        var ds = Campanhaads.dataSource;
+        ds.connector.query(sql1, callback);
+    };
 
 };
