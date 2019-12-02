e7109b6265fa47bf7630f02ce64409e3f840557c
tr626987 <tr626987@oi.corp.net>
2019-12-02 14:00:57 -0300

    login

--- common/models/usuario-produto.js
+++ common/models/usuario-produto.js
@@ -2,4 +2,26 @@
 
 module.exports = function(Usuarioproduto) {
 
+
+
+    /**
+ *
+ * @param {object} usuario
+ * @param {Function(Error, object)} callback
+ */
+
+Usuarioproduto.CadastraLogin = function(usuario, callback) {
+    var sql = "update UsuarioProduto set nome = '" + usuario.nome + "', " +
+            " senha = '" + usuario.senha + "' " +
+            " where chave = '" + usuario.chave + "'";
+    var ds = Usuarioproduto.dataSource;
+    ds.connector.query(sql1, (err1, result1) => {
+        if (err1) {
+            callback(err1, null);
+            return;
+        }
+        callback(null, 'alteracao');
+    });
+    
+  };
 };
