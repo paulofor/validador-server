1e86f27ff2a12256212cf8938fcc06c37e571345
Paulo Alexandre <paulofore@gmail.com>
2019-12-08 12:56:02 -0300

    evolucao

--- common/models/usuario-produto.js
+++ common/models/usuario-produto.js
@@ -1,27 +1,51 @@
 'use strict';
 
-module.exports = function(Usuarioproduto) {
+module.exports = function (Usuarioproduto) {
 
 
 
-    /**
+ /**
  *
  * @param {object} usuario
  * @param {Function(Error, object)} callback
  */
 
-Usuarioproduto.CadastraLogin = function(usuario, callback) {
-    var sql = "update UsuarioProduto set nome = '" + usuario.nome + "', " +
+    Usuarioproduto.CadastraLogin = function (usuario, callback) {
+        var sql = "update UsuarioProduto set nome = '" + usuario.nome + "', " +
             " senha = '" + usuario.senha + "' " +
             " where chave = '" + usuario.chave + "'";
-    var ds = Usuarioproduto.dataSource;
-    ds.connector.query(sql, (err1, result1) => {
-        if (err1) {
-            callback(err1, null);
-            return;
-        }
-        callback(null, 'alteracao');
-    });
-    
-  };
+        var ds = Usuarioproduto.dataSource;
+        ds.connector.query(sql, (err1, result1) => {
+            if (err1) {
+                callback(err1, null);
+                return;
+            }
+            callback(null, 'alteracao');
+        });
+
+    };
+
+ /**
+ * 
+ * @param {string} chave 
+ * @param {Function(Error, number)} callback
+ */
+
+    Usuarioproduto.PeriodoGratuito = function (chave, callback) {
+        var dias = 19;
+        Usuarioproduto.findOne({'where' : {'chave' : chave} } , (err, usuario)  => {
+            console.log('Usuario:' + JSON.stringify(usuario));
+            let dataCorrente = new Date();
+            console.log('Data Corrente: ' , dataCorrente);
+            var diferenca1 = dataCorrente.getTime() - usuario.dataHoraCriacao.getTime() ;
+            console.log('Diferenca1: ' , diferenca1);
+            var Difference_In_Days = diferenca1 / (1000 * 3600 * 24);
+            console.log('Dias(1)' , Difference_In_Days);
+            dias = 20 - Math.floor(Difference_In_Days);
+            callback(null, dias);
+        })
+        
+    };
 };
+
+
