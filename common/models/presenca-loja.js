da43b4e3f90bba50ddb4719a0e8f1b679336d03f
tr626987 <tr626987@oi.corp.net>
2020-01-06 13:30:18 -0300

    evolucao

--- common/models/presenca-loja.js
+++ common/models/presenca-loja.js
@@ -2,17 +2,46 @@
 
 module.exports = function (Presencaloja) {
 
+
+    /*
+update PresencaLoja 
+set 
+quantidadeCampanha = (select count(*) from CampanhaAds where presencaLojaId = PresencaLoja.id) ,
+mediaConversao = (select avg(conversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) , 
+mediaTaxaConversao = (select avg(taxaConversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) ,
+mediaCustoConversao = (select avg(custoConversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) 
+    */
+
+
     /**
      *
      * @param {Function(Error, object)} callback
      */
+     Presencaloja.CalculaMetrica = function (callback) {
+        //var result;
+        var sql = " update PresencaLoja " +
+            " set " +
+            " quantidadeCampanha = (select count(*) from CampanhaAds where presencaLojaId = PresencaLoja.id) , " +
+            " mediaConversao = (select avg(conversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) , " +
+            " mediaTaxaConversao = (select avg(taxaConversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) , " +
+            " mediaCustoConversao = (select avg(custoConversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) ";
+        var ds = Presencaloja.dataSource;
+        ds.connector.query(sql, callback);
+    };
 
-    Presencaloja.CalculaMetrica = function (callback) {
+    /**
+    *
+    * @param {number} idProjeto
+    * @param {Function(Error, object)} callback
+    */
+    Presencaloja.CalculaMetricaProjeto = function (idProjeto, callback) {
         var result;
         var sql = " update PresencaLoja " +
-            " set " + 
-            " quantidadeCampanha = (select count(*) from CampanhaAds where presencaLojaId = PresencaLoja.id) " +
-            " mediaConversao = (select avg(conversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) ";
+            " set " +
+            " quantidadeCampanha = (select count(*) from CampanhaAds where presencaLojaId = PresencaLoja.id) , " +
+            " mediaConversao = (select avg(conversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) , " +
+            " mediaTaxaConversao = (select avg(taxaConversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) , " +
+            " mediaCustoConversao = (select avg(custoConversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) ";
         var ds = Presencaloja.dataSource;
         ds.connector.query(sql, callback);
     };
