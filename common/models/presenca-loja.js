b5bd22e62d6fa46333a5fb116b6cea65bf938b26
tr626987 <tr626987@oi.corp.net>
2020-01-06 10:50:27 -0300

    metricas de presenca loja

--- common/models/presenca-loja.js
+++ common/models/presenca-loja.js
@@ -1,5 +1,20 @@
 'use strict';
 
-module.exports = function(Presencaloja) {
+module.exports = function (Presencaloja) {
+
+    /**
+     *
+     * @param {Function(Error, object)} callback
+     */
+
+    Presencaloja.CalculaMetrica = function (callback) {
+        var result;
+        var sql = " update PresencaLoja " +
+            " set " + 
+            " quantidadeCampanha = (select count(*) from CampanhaAds where presencaLojaId = PresencaLoja.id) " +
+            " mediaConversao = (select avg(conversao) from CampanhaAds where presencaLojaId = PresencaLoja.id) ";
+        var ds = Presencaloja.dataSource;
+        ds.connector.query(sql, callback);
+    };
 
 };
