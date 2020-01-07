82a37ff016659127a3e94becda14aced4463c40f
tr626987 <tr626987@oi.corp.net>
2020-01-07 11:46:35 -0300

    metricas de campanha ok

--- common/models/anuncio-aplicativo.js
+++ common/models/anuncio-aplicativo.js
@@ -2,6 +2,22 @@
 
 module.exports = function (Anuncioaplicativo) {
 
+
+    /*
+    update AnuncioAplicativo 
+    set
+    quantidadeCampanha = (select count(*) from CampanhaAds where anuncioAplicativoId = AnuncioAplicativo.id), 
+    mediaImpressao = (select avg(quantidadeImpressao)
+    from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id),
+    mediaCtr = (select avg(ctr) 
+    from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id),
+    mediaCustoConversao = (select avg(custoConversao) 
+    from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id), 
+    mediaCpc = (select avg(cpcMedio) 
+    from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id)
+    */
+
+
     /**
      *
      * @param {Function(Error, string)} callback
@@ -20,6 +36,50 @@ module.exports = function (Anuncioaplicativo) {
             " from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id)";
         var ds = Anuncioaplicativo.dataSource;
         ds.connector.query(sql, callback);
+    };
+
+    /**
+    *
+    * @param {Function(Error, object)} callback
+    */
+    Anuncioaplicativo.CalculaMetrica = function (callback) {
+        var sql = " update AnuncioAplicativo " +
+            " set " +
+            " quantidadeCampanha = (select count(*) from CampanhaAds where anuncioAplicativoId = AnuncioAplicativo.id), " +
+            " mediaImpressao = (select avg(quantidadeImpressao) " +
+            " from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id), " +
+            " mediaCtr = (select avg(ctr) " +
+            " from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id), " +
+            " mediaCustoConversao = (select avg(custoConversao) " +
+            " from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id), " +
+            " mediaCpc = (select avg(cpcMedio) " +
+            " from AnuncioAplicacaoResultado where AnuncioAplicacaoResultado.anuncioAplicativoId = AnuncioAplicativo.id)";
+        var ds = Anuncioaplicativo.dataSource;
+        ds.connector.query(sql, callback);
+    };
+
+
+    /**
+    * Por enquanto esta geral
+    * @param {number} idCampanha
+    * @param {Function(Error, object)} callback
+    */
+    Anuncioaplicativo.CalculaMetricaCampanha = function (idCampanha, callback) {
+        var result;
+        // TODO
+        callback(null, result);
+    };
+
+
+    /**
+    * Ainda nao implementada
+    * @param {number} idProjeto
+    * @param {Function(Error, object)} callback
+    */
+    Anuncioaplicativo.CalculaMetricaProjeto = function (idProjeto, callback) {
+        var result;
+        // TODO
+        callback(null, result);
     };
 
 };
--- common/models/setup-campanha.js
+++ common/models/setup-campanha.js
@@ -3,6 +3,18 @@
 module.exports = function (Setupcampanha) {
 
 
+    /*
+    update SetupCampanha
+    set
+    quantidadeCampanha = (select count(*) from CampanhaAds where setupCampanhaId = SetupCampanha.id) ,
+    mediaCustoConversao = (select avg(custoConversao) 
+    from CampanhaAds where CampanhaAds.setupCampanhaId = SetupCampanha.id), 
+    mediaCustoTotal = (select avg(orcamentoTotalExecutado) 
+    from CampanhaAds where CampanhaAds.setupCampanhaId = SetupCampanha.id)
+    */
+
+
+
     /**
      *
      * @param {Function(Error, string)} callback
@@ -10,6 +22,33 @@ module.exports = function (Setupcampanha) {
     Setupcampanha.CalculaMedias = function (callback) {
         var resultado;
         // TODO
-        callback(null, resultado);
+        callback(null, 'não implementado');
+    };
+
+    /**
+    *
+    * @param {Function(Error, object)} callback
+    */
+    Setupcampanha.CalculaMetrica = function (callback) {
+        var sql = " update SetupCampanha " +
+            " set " +
+            " quantidadeCampanha = (select count(*) from CampanhaAds where setupCampanhaId = SetupCampanha.id) , " +
+            " mediaCustoConversao = (select avg(custoConversao)  " +
+            " from CampanhaAds where CampanhaAds.setupCampanhaId = SetupCampanha.id), " +
+            " mediaCustoTotal = (select avg(orcamentoTotalExecutado) " +
+            " from CampanhaAds where CampanhaAds.setupCampanhaId = SetupCampanha.id)" ;
+        var ds = Setupcampanha.dataSource;
+        ds.connector.query(sql, callback);
+    };
+
+    /**
+    * por enquanto nao implementada
+    * @param {number} idCampanha
+    * @param {Function(Error, object)} callback
+    */
+    Setupcampanha.CalculaMetricaCampanha = function (idCampanha, callback) {
+        var result;
+        // TODO
+        callback(null, 'não implementado');
     };
 };
