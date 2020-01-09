1e0ea1c3cf54ea284a2b07593870870ee5c8fe23
tr626987 <tr626987@oi.corp.net>
2020-01-09 12:51:47 -0300

    versao para uso

--- common/models/custo-mes.js
+++ common/models/custo-mes.js
@@ -4,6 +4,19 @@ module.exports = function (Customes) {
 
 
     /**
+     * Criar dados com projeto null, totalizacao para meses
+     * @param {Function(Error, object)} callback
+     */
+    Customes.CriaAno = function (ano, callback) {
+        for (let i = 1; i <= 12; i++) {
+            let item = { 'mes': i, 'ano': ano };
+            Customes.create(item);
+        };
+        callback(null, { 'concluido': 'ok' });
+    };
+
+
+    /**
     * Cria itens para um projeto no ano 12 + 1
     * @param {number} ano 
     * @param {number} idProjeto 
@@ -43,27 +56,27 @@ module.exports = function (Customes) {
     Customes.AtualizaCustoMes = function (mes, ano, callback) {
         var ds = Customes.dataSource;
         var sqlSemProjeto = "update CustoMes " +
-            " set valorCampanha = " + 
+            " set valorCampanha = " +
             " ( " +
-            " select COALESCE(sum(orcamentoTotalExecutado),0)  " + 
-            " from CampanhaAds " + 
+            " select COALESCE(sum(orcamentoTotalExecutado),0)  " +
+            " from CampanhaAds " +
             " where " +
             " month(dataFinal) = " + mes + " and year(dataFinal) = " + ano +
             " ) " +
             " where mes = " + mes + " and ano = " + ano + " and projetoMySqlId is null";
-        ds.connector.query(sqlSemProjeto,(err,result) => {
+        ds.connector.query(sqlSemProjeto, (err, result) => {
             //console.log('err:' , err);
         });
         var sqlComProjeto = "update CustoMes " +
-                " set valorCampanha = " + 
-                " ( " +
-                " select COALESCE(sum(orcamentoTotalExecutado),0)  " + 
-                " from CampanhaAds " + 
-                " where CampanhaAds.projetoMySqlId = CustoMes.projetoMySqlId " +
-                " and month(dataFinal) = " + mes + " and year(dataFinal) = " + ano +
-                " ) " +
-                " where mes = " + mes + " and ano = " + ano + " and projetoMySqlId is not null";
-        ds.connector.query(sqlComProjeto,callback);
+            " set valorCampanha = " +
+            " ( " +
+            " select COALESCE(sum(orcamentoTotalExecutado),0)  " +
+            " from CampanhaAds " +
+            " where CampanhaAds.projetoMySqlId = CustoMes.projetoMySqlId " +
+            " and month(dataFinal) = " + mes + " and year(dataFinal) = " + ano +
+            " ) " +
+            " where mes = " + mes + " and ano = " + ano + " and projetoMySqlId is not null";
+        ds.connector.query(sqlComProjeto, callback);
     };
 
 
