06fce92feb2a3e49b0c1dddd9ec04d6cb98d0d07
Paulo Alexandre Lopes Forestieri <paulo.forestieri.stefanini@contratada.oi.net.br>
2020-01-22 12:37:07 -0300

    metodos de criacao de metricas-funil

--- common/models/valor-etapa-funil-campanha.js
+++ common/models/valor-etapa-funil-campanha.js
@@ -2,4 +2,16 @@
 
 module.exports = function(Valoretapafunilcampanha) {
 
+
+    /**
+ *
+ * @param {number} idCampanha
+ * @param {Function(Error, object)} callback
+ */
+
+Valoretapafunilcampanha.CriaEtapas = function(idCampanha, callback) {
+    var saida;
+    // TODO
+    callback(null, saida);
+  };
 };
