bd4118c0909d4e2d444b5ca22ba58a467610fd6f
Paulo Alexandre <paulofore@gmail.com>
2020-03-15 12:28:27 -0300

    plano-execucao recurso

--- common/models/versao-recurso.js
+++ common/models/versao-recurso.js
@@ -7,6 +7,73 @@ module.exports = function (Versaorecurso) {
 
 
 
+    Versaorecurso.ObtemPlanoDia = function (idContexto, callback) {
+        //console.log('Contexto' + idContexto);
+        var listaProcesso, diaSemana, semana, listaTempoExecucao;
+        var dataReferencia = new Date(new Date().toLocaleDateString() + " 00:00:00");
+        //var dataHoje = new Date();
+        //dataHoje = new Date(dataHoje.getTime() + 86400000);
+        var dataReferencia = new Date(dataHoje.toLocaleDateString() + " 00:00:00");
+        var posicao = dataReferencia.getDay() - 1;
+        //console.log('Posicao:', posicao);
+        if (posicao==-1) posicao = 6;
+        //console.log('Posicao: ' , posicao);
+        console.log('dataReferencia: ', dataReferencia , 'dataReferencia.getDay():' , dataReferencia.getDay());
+        app.models.DiaSemana.findOne({ 'where': { 'posicaoDia': posicao } }, (err, result1) => {
+            //console.log('Dia:', result1);
+            diaSemana = result1;
+            app.models.Semana.ObtemPorData(dataReferencia, (err, result2) => {
+                //console.log('Semana', result2);
+                semana = result2;
+                var filtroPlano = {
+                    "include": {
+                        "relation" : "versaoRecurso" , "scope" : { "include" : "recursoProduto"}
+                    },
+                    "where":
+                        {
+                            "and": [
+                                { "semanaId": semana.id },
+                                { "diaSemanaId": diaSemana.id },
+                                { "tempoEstimado": { "neq": 0 } },
+                                { "contextoId": idContexto }
+                            ]
+                        }
+                }
+                //console.log('Filtro: ' , JSON.stringify(filtroPlano));
+                app.models.PlanoExecucao.find(filtroPlano, (err, result3) => {
+                    listaProcesso = result3;
+                    var filtroTempo = {
+                        "include": [
+                            "processoNegocio", 
+                            "projetoMySql" , 
+                            {
+                                "relation" : "versaoRecurso" , "scope" : { "include" : "recursoProduto"}
+                            },
+                            { "relation" : "recursoProduto" , "scope" : 
+                                { "include" : {"relation" : "versaoRecursos" , "scope" : {"where" : {"emExecucao":"1"} } }  }
+                            }
+                        ],
+                        "where": {
+                            "and":
+                                [
+                                    { "semanaId": semana.id },
+                                    { "diaSemanaId": diaSemana.id },
+                                    { "contextoId": idContexto }
+                                ]
+                        },
+                        "order": "horaInicio DESC"
+                    }
+                    app.models.TempoExecucao.find(filtroTempo, (err, result) => {
+                        listaTempoExecucao = result;
+                        callback(null, listaProcesso, diaSemana, semana, listaTempoExecucao);
+                    })
+                })
+            })
+        });
+    };
+
+
+
 
     /*
     update VersaoRecurso
