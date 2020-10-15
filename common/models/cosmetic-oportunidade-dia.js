5cdf85044c22a44d5fd5dbea0dddf3ccc8c6ab02
Paulo Alexandre <paulofore@gmail.com>
2020-10-15 14:23:21 -0300

    sprint1

--- common/models/cosmetic-oportunidade-dia.js
+++ common/models/cosmetic-oportunidade-dia.js
@@ -20,5 +20,15 @@ module.exports = function (Cosmeticoportunidadedia) {
         callback(null,saida);
     };
 
-
+    Cosmeticoportunidadedia.ListaAppComputador = function (callback) {
+        //var lista = {[]};
+        var saida = [{"precoVendaAtual": 0,
+        "precoVendaAnterior": 0,
+        "nomeProduto": "Trocar port",
+        "nomeLojaVirtual": "-",
+        "imagemProduto": "https://images-americanas.b2w.io/produtos/01/00/img7/253315/1/253315174G1.jpg",
+        "dataUltimaPrecoAnterior": "1900-01-01T00:00:00.000Z",
+        "percentualAjusteVenda": 0}]
+        callback(null,saida);
+    };
 };
